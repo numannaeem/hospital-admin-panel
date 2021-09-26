e0ea94613f74695e0e4b449e9282a646cee9eaea
znapci <znapci@gmail.com>
2021-09-26 19:15:07 +0530

    Some betterment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import Sidebar from "./components/Sidebar";
-import { Flex } from "@chakra-ui/layout";
-import { BrowserRouter } from "react-router-dom";
+import Sidebar from './components/Sidebar'
+import { Flex } from '@chakra-ui/layout'
+import { BrowserRouter } from 'react-router-dom'
 
-function App() {
+function App () {
   return (
     <BrowserRouter>
       <Flex>
         <Sidebar />
       </Flex>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Flex, Text, IconButton, Divider, Avatar, Heading } from "@chakra-ui/react";
+import React, { useState } from 'react'
+import { Flex, Text, IconButton, Divider, Avatar, Heading, Tag } from '@chakra-ui/react'
 import {
   FiMenu,
   FiHome,
@@ -7,108 +7,113 @@ import {
   FiUser,
   FiDollarSign,
   FiBriefcase,
-  FiSettings,
-} from "react-icons/fi";
-import { IoPawOutline } from "react-icons/io5";
-import NavItem from "../components/NavItem";
-import { useLocation } from "react-router";
+  FiSettings
+} from 'react-icons/fi'
+import { IoPawOutline } from 'react-icons/io5'
+import NavItem from '../components/NavItem'
+import { useLocation } from 'react-router'
 
-export default function Sidebar() {
-  const location = useLocation();
-  const isActive = (routeName) => routeName === location.pathname;
-  const [navSize, changeNavSize] = useState("large");
+export default function Sidebar () {
+  const location = useLocation()
+  const isActive = (routeName) => routeName === location.pathname
+  const [navSize, changeNavSize] = useState('large')
   return (
     <Flex
-      pos="sticky"
-      left="5"
-      my="2.5vh"
-      backgroundColor="telegram.50"
-      boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-      borderRadius={navSize === "small" ? "15px" : "30px"}
-      w={navSize === "small" ? "75px" : "200px"}
-      flexDir="column"
-      justifyContent="space-between"
+      pos='sticky'
+      left='5'
+      my='2.5vh'
+      p='2'
+      backgroundColor='telegram.50'
+      boxShadow='0 4px 12px 0 rgba(0, 0, 0, 0.05)'
+      borderRadius='lg'
+      w={navSize === 'small' ? '75px' : '200px'}
+      transition='all'
+      transitionDuration='0.2s'
+      flexDir='column'
+      justifyContent='space-between'
     >
       <Flex
-        p="5%"
-        flexDir="column"
-        w="100%"
-        alignItems={navSize === "small" ? "center" : "flex-start"}
-        as="nav"
+        p='5%'
+        flexDir='column'
+        w='100%'
+        alignItems={navSize === 'small' ? 'center' : 'flex-start'}
+        as='nav'
       >
-        <IconButton
-          background="none"
-          mt={5}
-          _hover={{ background: "none" }}
-          icon={<FiMenu />}
-          onClick={() => {
-            if (navSize === "small") changeNavSize("large");
-            else changeNavSize("small");
-          }}
-        />
+        <Tag background='transparent'>
+          <IconButton
+            background='none'
+            mt={5}
+            _hover={{ background: 'none' }}
+            icon={<FiMenu />}
+            onClick={() => {
+              if (navSize === 'small') changeNavSize('large')
+              else changeNavSize('small')
+            }}
+          />
+        </Tag>
         <NavItem
           navSize={navSize}
           icon={FiHome}
-          title="Dashboard"
-          description="This is the description for the dashboard."
-          active={isActive("/dashboard")}
+          title='Dashboard'
+          description='This is the description for the dashboard.'
+          active={isActive('/dashboard')}
         />
         <NavItem
           navSize={navSize}
           icon={FiCalendar}
-          title="Calendar"
-          active={isActive("/calendar")}
+          title='Calendar'
+          active={isActive('/calendar')}
         />
         <NavItem
           navSize={navSize}
           icon={FiUser}
-          title="Clients"
-          active={isActive("/clients")}
+          title='Clients'
+          active={isActive('/clients')}
         />
         <NavItem
           navSize={navSize}
           icon={IoPawOutline}
-          title="Animals"
-          active={isActive("/animals")}
+          title='Animals'
+          active={isActive('/animals')}
         />
         <NavItem
           navSize={navSize}
           icon={FiDollarSign}
-          title="Stocks"
-          active={isActive("/stocks")}
+          title='Stocks'
+          active={isActive('/stocks')}
         />
         <NavItem
           navSize={navSize}
           icon={FiBriefcase}
-          title="Reports"
-          active={isActive("/reports")}
+          title='Reports'
+          active={isActive('/reports')}
         />
         <NavItem
           navSize={navSize}
           icon={FiSettings}
-          title="Settings"
-          active={isActive("/settings")}
+          title='Settings'
+          active={isActive('/settings')}
         />
       </Flex>
 
       <Flex
-        p="5%"
-        flexDir="column"
-        w="100%"
-        alignItems={navSize === "small" ? "center" : "flex-start"}
+        p='5%'
+        flexDir='column'
+        w='100%'
+        alignItems={navSize === 'small' ? 'center' : 'flex-start'}
         mb={4}
       >
-        <Divider display={navSize === "small" ? "none" : "flex"} />
-        <Flex mt={4} align="center">
-          <Avatar size="sm" src="avatar-1.jpg" />
-          <Flex flexDir="column" ml={4} display={navSize === "small" ? "none" : "flex"}>
-            <Heading as="h3" size="sm">
+        <Divider display={navSize === 'small' ? 'none' : 'flex'} />
+        <Flex mt={4} align='center'>
+          <Avatar size='sm' src='avatar-1.jpg' />
+          <Flex flexDir='column' ml={4} display={navSize === 'small' ? 'none' : 'flex'}>
+            <Heading as='h3' size='sm'>
               Sylwia Weller
             </Heading>
-            <Text color="gray">Admin</Text>
+            <Text color='gray'>Admin</Text>
           </Flex>
         </Flex>
       </Flex>
     </Flex>
-  );
+  )
 }
